426f4dc8baae375f8857063cbc16de102e6843b5
Ankit Mehrotra <ankit_mehrotra85@yahoo.com>
2022-01-24 22:53:23 +0100

    changes to codebase

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import image from './images/fuku.jpg'
+function App() {
+  return (
+    <div className="App">
+      <h2>My Name is Fuku.Here is my image</h2>
+      <img className="img-home" src={image} alt="Fuku pic" />
+    </div>
+  );
+}
+
+export default App;
